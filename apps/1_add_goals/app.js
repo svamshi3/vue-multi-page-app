c73866a32064ced55b73f667ea9b36c8b58423fb
svamshikrishna3 <3vamshi@gmail.com>
2021-06-12 18:20:15 +0530

    first vue app instance

--- apps/1_add_goals/app.js
+++ apps/1_add_goals/app.js
@@ -1,12 +1,34 @@
-const InputEl = document.querySelector('input');
-const ButtonEl = document.querySelector('button');
-const ulEl = document.querySelector('ul');
+// const InputEl = document.querySelector('input');
+// const ButtonEl = document.querySelector('button');
+// const ulEl = document.querySelector('ul');
 
-ButtonEl.addEventListener('click', () => {
-  console.log(InputEl.value);
-  const liEl = document.createElement('li');
-  liEl.textContent = InputEl.value;
-  ulEl.appendChild(liEl)
-  InputEl.value ="";
+// ButtonEl.addEventListener('click', () => {
+//   console.log(InputEl.value);
+//   const liEl = document.createElement('li');
+//   liEl.textContent = InputEl.value;
+//   ulEl.appendChild(liEl)
+//   InputEl.value ="";
 
-});
+// });
+
+console.log(Vue);
+
+const vueConfig = {
+  data() {
+    return {
+      enteredValue: '',
+      goalsList: []
+    }
+  },
+  methods: {
+    handleInput(e) {
+      this.enteredValue = e.target.value;
+    },
+    addGoal() {
+      this.goalsList.push(this.enteredValue)
+      this.enteredValue = '';
+    }
+  }
+};
+
+Vue.createApp(vueConfig).mount('#app');
